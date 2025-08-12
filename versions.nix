a3597bdf894612261e6ce7aaa97c1249f2c2f08d
miniluz <javiermelon4fu@gmail.com>
2025-08-12 17:41:30 +0200

    Add 25.4.0 and 25.5.0

--- versions.nix
+++ versions.nix
@@ -1,15 +1,17 @@
 let
-  makeVersion = version: npmDepsHash: dependencyDir: {
+  makeVersion = version: dependencyDir: {
     name = version;
     value = {
-      inherit npmDepsHash dependencyDir;
+      inherit dependencyDir;
     };
   };
 
-  makeCurrentVersion = version: npmDepsHash: makeVersion version npmDepsHash ".";
-  makeOldVersion = version: npmDepsHash: makeVersion version npmDepsHash "./versions/${version}";
+  makeCurrentVersion = version: makeVersion version ".";
+  makeOldVersion = version: makeVersion version "./versions/${version}";
 in
 builtins.listToAttrs [
-  (makeCurrentVersion "25.7.1" "sha256-AN0comIgRz3fFYu7UV2Mk5d4szrWM5sCLD/AwZsHqRg=")
-  (makeOldVersion "25.6.1" "sha256-HaOhKSfkFC4PAvOhMbkKK4M3kS/vY559I9vv6bQRT2Q=")
+  (makeCurrentVersion "25.7.1")
+  (makeOldVersion "25.6.1")
+  (makeOldVersion "25.5.0")
+  (makeOldVersion "25.4.0")
 ]
